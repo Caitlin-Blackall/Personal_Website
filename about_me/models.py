4db454ad7adc0260ade95442c3fe05ba8be1c7db
Caitlin B <caitlin.blackall@gmail.com>
2023-07-24 12:37:19 +0200

    fixed image display problems

--- about_me/models.py
+++ about_me/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class Profile(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.FilePathField(path="/img")
+    #image = models.FilePathField(path="/about_me/img")
+    image = models.ImageField(upload_to='about_me/img')
--- projects/urls.py
+++ projects/urls.py
@@ -5,3 +5,4 @@ urlpatterns = [
     path("", views.project_index, name="project_index"),
     path("<int:pk>/", views.project_detail, name="project_detail"),
 ]
+
