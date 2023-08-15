37251a1326e30868793bef0ad42cf35b0f82cdf5
Caitlin B <caitlin.blackall@gmail.com>
2023-08-15 16:40:37 +0200

    changed AWS to in progress section, altered layout, created database for works in progress

--- in_progress/admin.py
+++ in_progress/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from in_progress.models import In_Progress
 
 # Register your models here.
+class In_ProgressAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(In_Progress, In_ProgressAdmin)
--- in_progress/views.py
+++ in_progress/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from in_progress.models import In_Progress
 
 # Create your views here.
 def in_progress(request):
-    return render(request, 'in_progress.html')
+    posts = In_Progress.objects.all().order_by('-created_on')
+    context = {
+        "posts": posts,
+    }
+    return render(request, 'in_progress.html', context)
